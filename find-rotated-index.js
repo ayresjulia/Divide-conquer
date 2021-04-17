949d864c073599158eb4f1c2261228164b9a1197
Julia Ayres <j.bobrineva@gmail.com>
2021-04-17 16:37:09 -0400

    passed test for find-rotated-index

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,5 +1,15 @@
-function findRotatedIndex() {
- 
+function findRotatedIndex (arr, val, leftIdx = 0, rightIdx = arr.length - 1) {
+	while (leftIdx <= rightIdx) {
+		if (arr[leftIdx] == val) {
+			return leftIdx;
+		} else if (arr[rightIdx] == val) {
+			return rightIdx;
+		}
+		leftIdx++;
+		rightIdx--;
+	}
+
+	return -1;
 }
 
-module.exports = findRotatedIndex
+module.exports = findRotatedIndex;
