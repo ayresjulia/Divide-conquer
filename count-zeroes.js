427fc61179d1f99aae8076744198bc1b44b9f40f
Julia Ayres <j.bobrineva@gmail.com>
2021-04-20 15:48:33 -0400

    passed tests for count-zeroes

--- count-zeroes.js
+++ count-zeroes.js
@@ -1,7 +1,24 @@
-function countZeroes (arr, val = 0, leftIdx = 0, rightIdx = arr.length - 1) {}
+function countZeroes (arr, leftIdx = 0, rightIdx = arr.length - 1) {
+	if (arr[leftIdx] === 1 && arr[rightIdx] === 1) {
+		return 0;
+	}
 
-// module.exports = countZeroes;
+	while (leftIdx <= rightIdx) {
+		let midIdx = Math.floor((rightIdx + leftIdx) / 2);
+		let midVal = arr[midIdx];
+		if (midVal > arr[rightIdx]) {
+			leftIdx = midIdx + 1;
+		} else if (midVal === arr[rightIdx] && arr[midIdx - 1] === arr[rightIdx]) {
+			rightIdx = midIdx - 1;
+		} else {
+			return arr.length - midIdx;
+		}
+	}
+}
 
-console.log(countZeroes([ 1, 0, 0, 0, 1, 1 ])); // 3
-console.log(countZeroes([ 0, 0, 0, 1, 1 ])); // 3
+module.exports = countZeroes;
+
+// console.log(countZeroes([ 1, 1, 1, 1, 0, 0 ])); // 2
+// console.log(countZeroes([ 1, 0, 0, 0, 0 ])); // 4
+// console.log(countZeroes([ 0, 0, 0 ])); // 3
 // console.log(countZeroes([ 1, 1, 1, 1 ])); // 0
