881069f0feeb89ec2e9c49dffab4cecff0a0ac9f
Julia Ayres <j.bobrineva@gmail.com>
2021-04-20 14:13:39 -0400

    passed tests for find-rotated-index, deleted count-zeroes and sorted-frequency code

--- find-rotated-index.js
+++ find-rotated-index.js
@@ -1,14 +1,17 @@
 function findRotatedIndex (arr, val, leftIdx = 0, rightIdx = arr.length - 1) {
 	while (leftIdx <= rightIdx) {
-		if (arr[leftIdx] == val) {
-			return leftIdx;
-		} else if (arr[rightIdx] == val) {
-			return rightIdx;
+		let middleIdx = Math.floor((leftIdx + rightIdx) / 2);
+		let middleVal = arr[middleIdx];
+		if (middleVal < val) {
+			leftIdx = middleIdx + 1;
+		} else if (middleVal > val && arr[leftIdx] > val) {
+			leftIdx = middleIdx + 1;
+		} else if (middleVal > val) {
+			rightIdx = middleIdx - 1;
+		} else {
+			return middleIdx;
 		}
-		leftIdx++;
-		rightIdx--;
 	}
-
 	return -1;
 }
 
