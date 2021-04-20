03637942fc94e27c68b23c9e157f6847e49555af
Julia Ayres <j.bobrineva@gmail.com>
2021-04-20 16:14:21 -0400

    passed tests for sorted-frequency

--- count-zeroes.js
+++ count-zeroes.js
@@ -17,8 +17,3 @@ function countZeroes (arr, leftIdx = 0, rightIdx = arr.length - 1) {
 }
 
 module.exports = countZeroes;
-
-// console.log(countZeroes([ 1, 1, 1, 1, 0, 0 ])); // 2
-// console.log(countZeroes([ 1, 0, 0, 0, 0 ])); // 4
-// console.log(countZeroes([ 0, 0, 0 ])); // 3
-// console.log(countZeroes([ 1, 1, 1, 1 ])); // 0
--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,3 +1,36 @@
-function sortedFrequency (arr, val) {}
+function sortedFrequency (arr, val) {
+	let first = firstOcc(arr, val);
+	let last = lastOcc(arr, val);
+	if (first === -1 && last === -1) return -1;
+	return first === -1 || last === -1 ? 1 : last - first + 1;
+}
+
+function firstOcc (arr, val, leftIdx = 0, rightIdx = arr.length - 1) {
+	while (leftIdx <= rightIdx) {
+		let midIdx = Math.floor((rightIdx + leftIdx) / 2);
+		if (midIdx === 0 || (val > arr[midIdx - 1] && arr[midIdx] === val)) {
+			return midIdx;
+		} else if (val > arr[midIdx]) {
+			leftIdx = midIdx + 1;
+		} else {
+			rightIdx = midIdx - 1;
+		}
+	}
+	return -1;
+}
+
+function lastOcc (arr, val, leftIdx = 0, rightIdx = arr.length - 1) {
+	while (leftIdx <= rightIdx) {
+		let midIdx = Math.floor((rightIdx + leftIdx) / 2);
+		if (midIdx === 0 || (val < arr[midIdx + 1] && arr[midIdx] === val)) {
+			return midIdx;
+		} else if (val < arr[midIdx]) {
+			rightIdx = midIdx - 1;
+		} else {
+			leftIdx = midIdx + 1;
+		}
+	}
+	return -1;
+}
 
 module.exports = sortedFrequency;
